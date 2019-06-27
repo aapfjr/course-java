58f64856125f6834771520864eb6033483826cbe
Amarildo Junior <aapfjr@gmail.com>
2019-06-26 23:08:18 -0300

    Exemplo de Herança no pacote: com.algaworks.adestramento!

--- src/com/algaworks/adestramento/Animal.java
+++ src/com/algaworks/adestramento/Animal.java
@@ -2,4 +2,29 @@ package com.algaworks.adestramento;
 
 public class Animal {
 
+	private String raca;
+	private String nome;
+	
+	public Animal() {
+	}
+	
+	public Animal(String raca, String nome) {
+		this.raca = raca;
+		this.nome = nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
--- src/com/algaworks/adestramento/Treinador.java
+++ src/com/algaworks/adestramento/Treinador.java
@@ -1,5 +1,24 @@
 package com.algaworks.adestramento;
 
-public class Treinador {
+public class Treinador extends Pessoa {
+
+	private String especialidade;
+
+	public void seApresentar() {
+		System.out.println(
+				"Prazer sou o " + getNome() + ", tenho " + getIdade() + " anos e sou " + getEspecialidade() + "!");
+	}
+
+	public void ensinarSentar(Animal animal) {
+		System.out.println(animal.getNome() + ", hora de sentar! ... Isso muito bem!");
+	}
+
+	public String getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
 
 }
