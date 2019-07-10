dc99ec34ff4e19852c458d95e961689c88fa5832
Amarildo Junior <aapfjr@gmail.com>
2019-07-09 22:51:18 -0300

    Inseri um exemplo de sobreposição nesta Classe.

--- src/com/algaworks/adestramento/Treinador.java
+++ src/com/algaworks/adestramento/Treinador.java
@@ -4,6 +4,11 @@ public class Treinador extends Pessoa {
 
 	private String funcao;
 
+	public void seApresentar() {
+		super.seApresentar();
+		System.out.println("Tenho a função de " + getFuncao() + "!");
+	}
+	
 	public String getFuncao() {
 		return funcao;
 	}
